353a441499c28e6d2e6a2daade4241b98e986be3
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2025-07-25 13:52:34 +0545

    updated

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use('/uploads', express.static(path.join(__dirname, 'Uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/v1/', require('./routes/index'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(errorHandler);
